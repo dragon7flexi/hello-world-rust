e20269058a2c1789addfde9606e5977e2dcf753b
dragon7Flexi <ayatoa2589@gmail.com>
2025-08-12 11:32:03 +0900

    add actix web hello world api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({ "msg": "hello world" }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
